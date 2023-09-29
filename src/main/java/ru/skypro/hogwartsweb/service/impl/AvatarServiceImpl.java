aa5f740792ea169c283f3de0b699139918c474a9
Levan244 <panlevan9@gmail.com>
2023-09-29 18:47:47 +0400

    init commit 4.4 repeatedly

--- src/main/java/ru/skypro/hogwartsweb/service/impl/AvatarServiceImpl.java
+++ src/main/java/ru/skypro/hogwartsweb/service/impl/AvatarServiceImpl.java
@@ -2,6 +2,8 @@ package ru.skypro.hogwartsweb.service.impl;
 
 
 import jakarta.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.querydsl.QPageRequest;
@@ -32,6 +34,7 @@ import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
 @Service
 public abstract class AvatarServiceImpl implements AvatarRepository{
+    private final Logger logger = LoggerFactory.getLogger(AvatarRepository.class);
     private final StudentService studentService;
     private final AvatarRepository avatarRepository;
     @Value("${avatars.dir.path}")
@@ -45,15 +48,18 @@ public abstract class AvatarServiceImpl implements AvatarRepository{
 
     @Override
     public Avatar findAvatar(Long studentId) {
+        logger.info("Was invoker method findAvatar for avatars");
         return avatarRepository.findById(studentId).orElseThrow(EntityNotFoundException::new);
     }
 
     @Override
     public Avatar findOrCreateAvatar(Long studentId) {
+        logger.info("Was invoker method findOrCreateAvatar for avatars");
         return avatarRepository.findById(studentId).orElse(new Avatar());
     }
 
     public void uploadAvatar(long studentId, MultipartFile avatarFile) throws IOException {
+        logger.info("Was invoker method uploadAvatar for avatars");
         if (avatarFile != null) {
             Student student = studentService.get(studentId);
             Path filePath = buildFilePath(student, avatarFile.getOriginalFilename());
@@ -66,6 +72,7 @@ public abstract class AvatarServiceImpl implements AvatarRepository{
                 BufferedInputStream bos = new BufferedInputStream(is, 1024);
 
             } catch (IOException e) {
+                logger.error("Avatars not Found, studentId = " + studentId);
                 throw new RuntimeException(e);
             }
             Avatar avatar = findOrCreateAvatar(studentId);
@@ -80,14 +87,17 @@ public abstract class AvatarServiceImpl implements AvatarRepository{
     }
 @Override
     public List<Avatar> getPage(Integer pageNumber, Integer pageSize) {
+    logger.info("Was invoker method getPage for avatars");
         return avatarRepository.findAll(QPageRequest.of(pageNumber - 1, pageSize)).getContent();
     }
 
     private String getExtensions(String fileName) {
+        logger.info("Was invoker method getExtensions for avatars");
         return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
 
     private Path buildFilePath(Student student, String fileName) {
+        logger.info("Was invoker method buildFilePath for avatars");
         return Path.of(avatarDir, student.getId() + "-" + student.getName() + "." +
                 getExtensions(Objects.requireNonNull(fileName)));
     }
